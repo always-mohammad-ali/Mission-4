a582535ab0c601f6a8d3d7dd85f7ae0c1e9a3618
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-13 00:16:59 +0600

    courses table partially done for courseEnrollment database

--- Module-19.5/queries.sql
+++ Module-19.5/queries.sql
@@ -29,3 +29,42 @@ select first_name, last_name, coalesce(phone, 'not provided') as phone_num from 
 
 
 
+create table courses(
+  course_id serial primary key,
+  course_title varchar(150) not null,
+  category varchar(50) not null,
+  price decimal(10, 2) not null,
+  instructor varchar(100),
+  published_year integer
+)
+
+insert into courses(course_id, course_title, category, price, instructor, published_year)
+values 
+(1, 'Complete SQL Bootcamp', 'Database', 49.99, 'John Carter', 2021),
+(2, 'Advanced JavaScript', 'Programming', 59.99, 'Sarah Miller', 2020),
+(3, 'Python for Data Science', 'Data Science', 69.99, 'David Kim', 2022),
+(4, 'Web Development with React', 'Programming', 54.99, 'Emily Stone', 2021),
+(5, 'Machine Learning Basics', 'AI', 79.99, 'Andrew Ng', 2019),
+(6, 'Cloud Computing Fundamentals', 'Cloud', 64.99, 'James Allen', 2020),
+(7, 'UI/UX Design Essentials', 'Design', 39.99, 'Laura Scott', 2022),
+(8, 'DevOps for Beginners', 'DevOps', 74.99, 'Michael Brown', 2023);
+
+
+--Question : 2 Show all courses ordered by price (highest to lowest) and limit the result to 5 courses.
+select course_title, price from courses order by price desc limit 5;
+
+
+--Question : 3 Display courses for page 2, assuming 3 courses per page, using LIMIT and OFFSET.
+--OFFSET = (Page Number - 1) × Items Per Page     LIMIT = Items Per Page
+
+select * from courses order by course_id limit 3 offset 3;
+select * from courses order by course_id limit 3 offset 6;
+select * from courses order by course_id limit 3;
+
+
+--Question : 4 Update the price of all courses in the Programming category by increasing it 10%.
+update courses set price = price * 1.10 where category = 'Programming';
+select course_title, price, category from courses where category = 'Programming'
+
+
+
